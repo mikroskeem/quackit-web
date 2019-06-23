e30427b9fd533cf29a8846be32360dde6e3b422c
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2019-06-23 17:52:03 +0300

    Make build script generate deploy-ready app.tar as well

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+set -e
 if [ ! -f "wasm_exec.js" ]; then
     cp -v "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
 fi
 
 ./goenv.sh go build -o main.wasm
+
+tar -cf app.tar \
+    index.html \
+    main.wasm \
+    wasm_exec.js
